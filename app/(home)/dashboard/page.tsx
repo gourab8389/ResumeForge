144d670175d1b830a7286604725e54ab6689965f
gourab8389 <deyg6988@gmail.com>
2025-01-21 01:57:32 +0530

    refactor authentication flow and update landing/dashboard layouts

---
+++ app/(home)/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+
+const DocumentPage = async () => {
+  const {getUser} = getKindeServerSession();
+const user = await getUser();
+
+  return (
+    <div className="flex items-center justify-center h-screen flex-col gap-5">
+      All resume
+
+      <div>
+        <h1>{user?.email}</h1>
+        <span>{user?.id}</span>
+      </div>  
+    </div>
+  )
+}
+
+export default DocumentPage
--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,16 +1,18 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 interface LandingLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const LandingLayout = (
-    { children }: LandingLayoutProps
-) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
-}
+const LandingLayout = async ({ children }: LandingLayoutProps) => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) {
+    redirect("/dashboard");
+  }
+  return <div>{children}</div>;
+};
 
 export default LandingLayout;
