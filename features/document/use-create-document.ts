f018cc498e786df4458b771801e287b9e52b3b21
gourab8389 <deyg6988@gmail.com>
2025-01-27 19:03:37 +0530

    implement useCreateDocument hook with error handling and success notifications; integrate API mutation for document creation

--- features/document/use-create-document.ts
+++ features/document/use-create-document.ts
@@ -1,10 +1,32 @@
 "use client"
 
-import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "@/hooks/use-toast";
+import { api } from "@/lib/hono-rpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
 
+type ResponseType = InferResponseType<typeof api.document.create.$post>;
+type RequestType = InferRequestType<typeof api.document.create.$post>["json"];
 
 const useCreateDocument = () => {
     const queryClient = useQueryClient();
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await api.document.create.$post({ json });
+            return await response.json();
+        },
+        onSuccess: (response) => {
+            console.log(response);
+            queryClient.invalidateQueries({ queryKey: ["documents"]});
+        },
+        onError: () => {
+            toast({
+                title: "Failed to create document",
+                description: "An error occurred while creating the document",
+                variant: "destructive"
+            })
+        }
+    })
 };
 
 
