515127d053752b00e4d90b3c212a9467ef1aded1
gourab8389 <deyg6988@gmail.com>
2025-01-27 18:38:00 +0530

    add QueryProvider to layout; implement useCreateDocument hook with react-query; create hono-rpc client for API interactions

--- features/document/use-create-document.ts
+++ features/document/use-create-document.ts
@@ -1,1 +1,11 @@
 "use client"
+
+import { useQueryClient } from "@tanstack/react-query";
+
+
+const useCreateDocument = () => {
+    const queryClient = useQueryClient();
+};
+
+
+export default useCreateDocument;
