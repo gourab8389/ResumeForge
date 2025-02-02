90dde90504efe643c4c01895ec5ab6e5c3a0dc8c
gourab8389 <deyg6988@gmail.com>
2025-02-02 21:06:18 +0530

    enhance ResumeItem component with thumbnail support and improved layout

--- app/(home)/_components/common/resume-item.tsx
+++ app/(home)/_components/common/resume-item.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 import { useCallback, useMemo } from "react";
+import { Dot, EllipsisVertical, FileText, Globe, Lock } from "lucide-react";
+import Image from "next/image";
 
 interface ResumeItemProps {
     documentId: string;
@@ -18,7 +20,7 @@ const ResumeItem = (
 
     const docDate = useMemo(()=>{
         if(!updatedAt) return null;
-        const formatetedDate = format(updatedAt, "MM dd, yyyy");
+        const formatetedDate = format(updatedAt, "MMM dd, yyyy");
         return formatetedDate;
     }, [updatedAt]);
     const gotoDoc = useCallback(() => {
@@ -28,9 +30,58 @@ const ResumeItem = (
     <div
     role="button"
     className="cursor-pointer max-w-[164px] w-full border rounded-lg transition-all h-[197px] hover:border-primary hover:shadow-md shadow-primary"
-    style={{border: themeColor ?? "#E5E7EB"}}
+    onClick={gotoDoc}
+    style={{borderColor: themeColor ?? "#E5E7EB"}}
     >
-      <div className="flex flex-col w-full h-full items-center rounded-lg justify-center bg-[#fdfdfd] dark:bg-secondary"></div>
+      <div className="flex flex-col w-full h-full items-center rounded-lg justify-center bg-[#fdfdfd] dark:bg-secondary">
+        <div className="w-full flex flex-1 px-1 pt-2">
+          <div className="w-full flex flex-1 bg-white dark:bg-gray-700 rounded-t-lg justify-center items-center">
+            {thumbnail ? (
+              <div className="relative w-full h-full rounded-t-lg overflow-hidden">
+                <Image 
+                src={thumbnail} 
+                alt={title} 
+                fill 
+                className="w-full h-full object-cover object-top rounded-t-lg"
+                />
+              </div>
+            ) : (
+              <FileText size="30px"/>
+            )}
+          </div>
+        </div>
+
+        {/* Body content */}
+        <div className="shrink w-full border-t pt-2 pb-[9px] px-[9px]">
+          <div className="flex items-center justify-between">
+            <h5 className="font-semibold text-sm mb-[2px] truncate block w-[200px]">
+              {title}
+            </h5>
+            <button className="text-muted-foreground">
+              <EllipsisVertical size="20px" />
+            </button>
+          </div>
+          <div className="flex items-center !text-[12px] font-medium text-muted-foreground">
+            <span className="flex items-center gap-[2px]">
+              {status === "private" ? (
+                <>
+                <Lock size="12px" />
+                Private  
+                </>
+              ) : (
+                <>
+                <Globe size="12px" className="text-primary"/>
+                Public
+                </>
+              )}
+            </span>
+            <Dot size="15px" />
+            <span>
+              {docDate}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
