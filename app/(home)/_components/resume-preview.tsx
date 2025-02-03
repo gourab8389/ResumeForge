d3c5b739399ac9ebcd8fc0c3d40db023f0ae494e
gourab8389 <deyg6988@gmail.com>
2025-02-04 01:25:12 +0530

    add PersonalInfo component and integrate it into ResumePreview; update theme color and loading state handling

--- app/(home)/_components/resume-preview.tsx
+++ app/(home)/_components/resume-preview.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useResumeContext } from "@/context/resume-info-provider";
+import { cn } from "@/lib/utils";
+import PersonalInfo from "./preview/personal-info";
+// import PersonalInfo from "@/components/preview/PersonalInfo";
+// import SummaryPreview from "@/components/preview/SummaryPreview";
+// import ExperiencePreview from "@/components/preview/ExperiencePreview";
+// import EducationPreview from "@/components/preview/EducationPreview";
+// import SkillPreview from "@/components/preview/SkillPreview";
 
 const ResumePreview = () => {
+  const { resumeInfo } = useResumeContext();
+  const isLoading = false;
+
   return (
-    <div>
-      preview section
+    <div
+      id="resume-preview-id"
+      className={cn(`
+        shadow-lg bg-white w-full flex-[1.02]
+        h-full p-10 !font-open-sans
+        dark:border dark:bg-card 
+        dark:border-b-gray-800 
+        dark:border-x-gray-800
+        `)}
+      style={{
+        borderTop: `13px solid ${resumeInfo?.themeColor}`,
+      }}
+    >
+      {/* {Personnal Info} */}
+      <PersonalInfo isLoading={isLoading} resumeInfo={resumeInfo} />
+
+      {/* {Summary} */}
+      {/* <SummaryPreview isLoading={isLoading} resumeInfo={resumeInfo} /> */}
+
+      {/* {Professional Exp} */}
+      {/* <ExperiencePreview isLoading={isLoading} resumeInfo={resumeInfo} /> */}
+
+      {/* {Educational Info} */}
+      {/* <EducationPreview isLoading={isLoading} resumeInfo={resumeInfo} /> */}
+
+      {/* {Skills} */}
+      {/* <SkillPreview isLoading={isLoading} resumeInfo={resumeInfo} /> */}
     </div>
-  )
-}
+  );
+};
 
 export default ResumePreview;
