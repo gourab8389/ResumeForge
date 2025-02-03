213f3a7250fa43c4192f7842fe7eeefa1944afc7
gourab8389 <deyg6988@gmail.com>
2025-02-03 20:23:11 +0530

    add ResumeForm and TopSection components; integrate them into EditResume for improved layout

--- app/(home)/_components/edit-resume.tsx
+++ app/(home)/_components/edit-resume.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import TopSection from './common/top-section';
+import ResumeForm from './common/resume-form';
 
 const EditResume = () => {
   return (
-    <div>
-      
+    <div className='relative w-full'>
+      <div className="w-full mx-auto max-w-7xl py-4 px-5">
+        <TopSection/>
+        <div className="w-full mt-1">
+            <div className="flex flex-col lg:flex-row items-start w-full py-3 gap-6">
+                {/* form section */}
+                <ResumeForm/>
+                {/* preview section */}
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
